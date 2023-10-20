7608ed76b2a4fe1f44198c4fea4f2f417ade46bd
minjeong <kmj121612@gmail.com>
2023-10-20 13:35:42 +0900

    2023.10.19

--- chap5/prob6/main.c
+++ chap5/prob6/main.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -8,7 +9,7 @@ int main(int argc, char *argv[])
    int fd, id;
    char c;
    struct student record;
-   if(argc<2){
+   if(argc < 2) {
 	   fprintf(stderr, "How to use: %s file\n", argv[0]);
 	   exit(1);
    }
@@ -20,7 +21,7 @@ int main(int argc, char *argv[])
 
    do{
 	   printf("\Enter StudentID to search:");
-	   if(scanf("%d", &id)==1){
+	   if(scanf("%d", &id) == 1){
 		   lseek(fd, (id-START_ID)*sizeof(record), SEEK_SET);
 		   if ((read(fd, (char *) &record, sizeof(record)) > 0) && (record.id != 0))
 			   printf("Name: %s\t StuID:%d\t Score:%d\n", record.name, record.id, record.score);
